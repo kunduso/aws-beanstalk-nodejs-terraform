6a25d274017bda084562fb60331f3dd2b48ecd8e
sourav kundu <kunduso@amazon.com>
2025-10-22 17:37:16 -0500

    feat: add load balancer URL output
    - Add load_balancer_url output for direct HTTP access
    - Keep existing application_name and environment_url outputs
    - Provide accessible URL format for deployed application

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "environment_url" {
   description = "Environment URL"
   value       = aws_elastic_beanstalk_environment.todo_env.endpoint_url
 }
+
+output "load_balancer_url" {
+  description = "Load balancer URL"
+  value       = "http://${aws_elastic_beanstalk_environment.todo_env.cname}"
+}
