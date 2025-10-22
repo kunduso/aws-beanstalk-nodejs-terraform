82b86e347632070e6daadc275835642952829018
sourav kundu <kunduso@amazon.com>
2025-10-22 17:21:29 -0500

    feat: add Node.js to-do application
    - Basic Express.js server with in-memory storage
    - CRUD operations for to-do items
    - Simple HTML interface with forms
    - Configured for Elastic Beanstalk deployment

---
+++ app/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let todos = [];
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>To-Do App</h1>
+    <form action="/add" method="post">
+      <input type="text" name="todo" placeholder="Add a new task" required>
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+      ${todos.map((todo, index) => `
+        <li>
+          ${todo}
+          <form style="display:inline" action="/delete/${index}" method="post">
+            <button type="submit">Delete</button>
+          </form>
+        </li>
+      `).join('')}
+    </ul>
+  `);
+});
+
+app.post('/add', (req, res) => {
+  todos.push(req.body.todo);
+  res.redirect('/');
+});
+
+app.post('/delete/:index', (req, res) => {
+  todos.splice(req.params.index, 1);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
