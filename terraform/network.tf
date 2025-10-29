0eb2d030d66292cc6063a430121b0af0808a3f19
sourav kundu <kunduso@amazon.com>
2025-10-29 09:15:43 -0500

    add dns configuration to vpc

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,6 +3,8 @@ module "vpc" {
   region                  = var.region
   enable_internet_gateway = true
   enable_nat_gateway      = true
+  enable_dns_support      = true
+  enable_dns_hostnames    = true
   vpc_cidr                = "10.20.30.0/24"
   subnet_cidr_public      = ["10.20.30.0/27", "10.20.30.32/27", "10.20.30.64/27"]
   subnet_cidr_private     = ["10.20.30.128/27", "10.20.30.160/27", "10.20.30.192/27"]
