fd62b0d59e670d6c5b5dceb1df0874946a5dd47a
sourav kundu <kunduso@amazon.com>
2025-12-29 16:20:46 -0600

    expanded cidrs

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,8 +6,8 @@ module "vpc" {
   enable_dns_support      = true
   enable_dns_hostnames    = true
   vpc_cidr                = "10.20.30.0/24"
-  subnet_cidr_public      = ["10.20.30.0/27", "10.20.30.32/27", "10.20.30.64/27"]
-  subnet_cidr_private     = ["10.20.30.128/27", "10.20.30.160/27", "10.20.30.192/27"]
+  subnet_cidr_public      = ["10.20.30.0/26", "10.20.30.64/26"]
+  subnet_cidr_private     = ["10.20.30.128/26", "10.20.30.192/26"]
   #CKV_TF_1: Ensure Terraform module sources use a commit hash
   #checkov:skip=CKV_TF_1: This is a self hosted module where the version number is tagged rather than the commit hash.
 }
