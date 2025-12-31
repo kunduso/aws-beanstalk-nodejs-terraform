b413eb76117d79ff7e94e0d3e92d16428099a9c8
Sourav Kundu <skundu.dev@gmail.com>
2025-12-31 12:07:33 -0600

    add documentation links for AWS resources in Terraform files. Fix for #6

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,5 @@
 # IAM Role for Beanstalk Service
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
 resource "aws_iam_role" "beanstalk_service" {
   name = "aws-elasticbeanstalk-service-role"
 
@@ -16,12 +17,14 @@ resource "aws_iam_role" "beanstalk_service" {
   })
 }
 
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "beanstalk_service" {
   role       = aws_iam_role.beanstalk_service.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
 }
 
 # IAM Role for EC2 Instances
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
 resource "aws_iam_role" "beanstalk_ec2" {
   name = "aws-elasticbeanstalk-ec2-role"
 
@@ -39,28 +42,33 @@ resource "aws_iam_role" "beanstalk_ec2" {
   })
 }
 
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "beanstalk_ec2_web" {
   role       = aws_iam_role.beanstalk_ec2.name
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
 }
 
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "beanstalk_ec2_worker" {
   role       = aws_iam_role.beanstalk_ec2.name
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
 }
 
 # CloudWatch Logs permissions for EC2 instances
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "beanstalk_ec2_cloudwatch" {
   role       = aws_iam_role.beanstalk_ec2.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
 # Enhanced health reporting permissions
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "beanstalk_service_health" {
   role       = aws_iam_role.beanstalk_service.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
 }
 
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_instance_profile
 resource "aws_iam_instance_profile" "beanstalk_ec2" {
   name = "aws-elasticbeanstalk-ec2-role"
   role = aws_iam_role.beanstalk_ec2.name
