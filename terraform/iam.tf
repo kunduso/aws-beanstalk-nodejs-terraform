75f564639a3cf9711ca5b69a4a9f8751a9e13cc4
Sourav Kundu <skundu.dev@gmail.com>
2025-12-31 13:15:40 -0600

    refactor IAM roles to use variable names for consistency and add CloudWatch permissions for Beanstalk. Fix for #16

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 # IAM Role for Beanstalk Service
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
 resource "aws_iam_role" "beanstalk_service" {
-  name = "aws-elasticbeanstalk-service-role"
+  name = "${var.name}-elasticbeanstalk-service-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -26,7 +26,7 @@ resource "aws_iam_role_policy_attachment" "beanstalk_service" {
 # IAM Role for EC2 Instances
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
 resource "aws_iam_role" "beanstalk_ec2" {
-  name = "aws-elasticbeanstalk-ec2-role"
+  name = "${var.name}-elasticbeanstalk-ec2-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -54,11 +54,27 @@ resource "aws_iam_role_policy_attachment" "beanstalk_ec2_worker" {
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
 }
 
-# CloudWatch Logs permissions for EC2 instances
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
-resource "aws_iam_role_policy_attachment" "beanstalk_ec2_cloudwatch" {
-  role       = aws_iam_role.beanstalk_ec2.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+# Add minimal CloudWatch permissions for Beanstalk
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy
+resource "aws_iam_role_policy" "beanstalk_cloudwatch" {
+  name = "${var.name}-beanstalk-cloudwatch-logs"
+  role = aws_iam_role.beanstalk_ec2.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "arn:aws:logs:*:*:log-group:/aws/elasticbeanstalk/*"
+      }
+    ]
+  })
 }
 
 # Enhanced health reporting permissions
@@ -70,6 +86,6 @@ resource "aws_iam_role_policy_attachment" "beanstalk_service_health" {
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_instance_profile
 resource "aws_iam_instance_profile" "beanstalk_ec2" {
-  name = "aws-elasticbeanstalk-ec2-role"
+  name = "${var.name}-elasticbeanstalk-ec2-role"
   role = aws_iam_role.beanstalk_ec2.name
 }
